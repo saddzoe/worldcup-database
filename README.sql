c84ab04e5c2e5f9feb08bbce4893d4867b471319
Mohammed Olaiya <mohammedolaiya@gmail.com>
2023-03-11 22:04:12 -0500

    Update README.sql

--- README.sql
+++ README.sql
@@ -1,2 +1,22 @@
 # worldcup-database
-A project from freecodecamp from database
+#A project from freecodecamp from database
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
+-- Dumped by pg_dump version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE worldcup;
