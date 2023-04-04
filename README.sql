07ad0a3a1a6d132f63f900240d56c2bfad4f38d3
Mohammed Olaiya <mohammedolaiya@gmail.com>
2023-04-04 11:28:17 -0400

    Update README.sql

--- README.sql
+++ README.sql
@@ -74,3 +74,18 @@ CREATE TABLE public.games (
     winner_goals integer NOT NULL,
     opponent_goals integer NOT NULL
 );
+
+ALTER TABLE public.games OWNER TO freecodecamp;
+
+--
+-- Name: games_game_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+--
+
+CREATE SEQUENCE public.games_game_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
