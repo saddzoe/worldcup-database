aa311d8290d16302135ddb7c7199f95b965738e2
Mohammed Olaiya <mohammedolaiya@gmail.com>
2023-08-20 04:42:24 -0400

    Update README.sql

--- README.sql
+++ README.sql
@@ -248,3 +248,10 @@ ALTER TABLE ONLY public.teams
 
 ALTER TABLE ONLY public.games
     ADD CONSTRAINT games_opponent_id_fkey FOREIGN KEY (opponent_id) REFERENCES public.teams(team_id);
+
+--
+-- Name: games games_winner_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.games
+    ADD CONSTRAINT games_winner_id_fkey FOREIGN KEY (winner_id) REFERENCES public.teams(team_id);
